9c8d13fa29e97a91f26d7f028dcc0c2d2d033145
Ben Lo <blo@tribalscale.com>
2016-11-28 20:00:21 -0500

    BL/KQ - Add utility function that checks if String is null or empty.
    Signed-off-by: Keane Quibilan <kquibilan@tribalscale.com>

---
+++ app/src/main/java/com/tribalhacks/gamify/utils/StringUtils.java
@@ -0,0 +1,19 @@
+package com.tribalhacks.gamify.utils;
+
+
+public final class StringUtils {
+
+    private StringUtils() {
+        // no-op
+    }
+
+    /**
+     * Returns {@code true} if the String passed in is Empty or {@code null}.
+     *
+     * @param string The {@link String} to check.
+     * @return {@code true} if the String passed in is Empty or {@code null}.
+     */
+    public static boolean isEmptyOrNull(String string) {
+        return string == null || string.length() <= 0;
+    }
+}
